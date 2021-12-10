3e6d207b6dc20ccb1ad3d1f845fd2428952cc793
Ryry525 <ryanlin0228@gmail.com>
2021-12-10 15:43:23 -0500

    added objects with text for the game

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log("scripit is loading")
+
+let myText = {
+    id: 1,
+    story: "Ming's rank got lost in elo hell, please help him find his way out"
+    options: ["start playing adc", "start playing mid", "start playing jungle", "start playing top"]
+}
